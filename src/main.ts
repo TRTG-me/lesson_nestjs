f0e8b5e39b4b1e01f478fb072d44cc3c7bd2e09e
TORTUGA-me <mcgradysvt@gmail.com>
2024-10-01 12:08:05 +0300

    Init repository

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService)
+  await app.listen(3000);
+}
+bootstrap();
---
+++ src/moks/index.ts
@@ -0,0 +1,229 @@
+export const users = [
+    [
+        {
+          "_id": "66f94e5ff46bed13b54ff1c6",
+          "index": 0,
+          "guid": "074634b2-39c6-44bf-a064-871e07083d4b",
+          "isActive": true,
+          "balance": "$3,511.02",
+          "picture": "http://placehold.it/32x32",
+          "age": 30,
+          "eyeColor": "green",
+          "name": "Pena Mcclure",
+          "gender": "male",
+          "company": "ERSUM",
+          "email": "penamcclure@ersum.com",
+          "phone": "+1 (869) 558-3766",
+          "address": "992 Gerry Street, Newry, Wyoming, 2286",
+          "about": "Sint eiusmod officia pariatur cupidatat fugiat excepteur. Cillum esse do laboris dolore ipsum voluptate cupidatat reprehenderit adipisicing. Do et excepteur reprehenderit aliquip commodo non excepteur aliqua occaecat nisi.\r\n",
+          "registered": "2015-10-30T04:31:01 -02:00",
+          "latitude": -30.894822,
+          "longitude": 87.627699,
+          "tags": [
+            "labore",
+            "anim",
+            "eiusmod",
+            "excepteur",
+            "dolor",
+            "aute",
+            "enim"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Wagner Franklin"
+            },
+            {
+              "id": 1,
+              "name": "Rosario Workman"
+            },
+            {
+              "id": 2,
+              "name": "Sophia Edwards"
+            }
+          ],
+          "greeting": "Hello, Pena Mcclure! You have 6 unread messages.",
+          "favoriteFruit": "strawberry"
+        },
+        {
+          "_id": "66f94e5ff1c5d0d186cd7b7c",
+          "index": 1,
+          "guid": "83c8d8f2-d9a1-4d22-a8d2-f3fd50276c4d",
+          "isActive": true,
+          "balance": "$1,911.76",
+          "picture": "http://placehold.it/32x32",
+          "age": 39,
+          "eyeColor": "blue",
+          "name": "Schultz Pacheco",
+          "gender": "male",
+          "company": "ROBOID",
+          "email": "schultzpacheco@roboid.com",
+          "phone": "+1 (917) 555-3871",
+          "address": "230 Claver Place, Jamestown, Florida, 8463",
+          "about": "Nostrud commodo ad mollit sunt nostrud nostrud ex est esse laboris officia quis deserunt exercitation. Elit fugiat non fugiat magna sunt quis duis laboris enim eu cupidatat. Minim pariatur Lorem id duis eiusmod elit. Laborum anim nisi tempor aliquip minim in excepteur id fugiat in. Velit nostrud aliquip incididunt cillum ipsum amet ipsum occaecat ullamco eiusmod in.\r\n",
+          "registered": "2022-01-22T09:51:26 -02:00",
+          "latitude": 28.68332,
+          "longitude": -35.081216,
+          "tags": [
+            "non",
+            "ea",
+            "duis",
+            "exercitation",
+            "quis",
+            "et",
+            "Lorem"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Potts Bush"
+            },
+            {
+              "id": 1,
+              "name": "Summer Terry"
+            },
+            {
+              "id": 2,
+              "name": "Faulkner Slater"
+            }
+          ],
+          "greeting": "Hello, Schultz Pacheco! You have 10 unread messages.",
+          "favoriteFruit": "strawberry"
+        },
+        {
+          "_id": "66f94e5fe40b7a01371b766d",
+          "index": 2,
+          "guid": "5cedaed9-876a-433f-a5e7-c2656ea1ae15",
+          "isActive": true,
+          "balance": "$1,920.64",
+          "picture": "http://placehold.it/32x32",
+          "age": 23,
+          "eyeColor": "green",
+          "name": "Patton Holcomb",
+          "gender": "male",
+          "company": "IMAGEFLOW",
+          "email": "pattonholcomb@imageflow.com",
+          "phone": "+1 (940) 597-2688",
+          "address": "456 Diamond Street, Marysville, Mississippi, 8060",
+          "about": "Fugiat officia excepteur adipisicing labore consequat consectetur mollit adipisicing dolore. Aliqua labore sint non dolor ex. Et culpa et aliqua cupidatat aliquip veniam. Mollit officia incididunt dolor duis sint id sunt dolor culpa non laborum minim magna reprehenderit. Nisi ex enim veniam est sint ex laborum duis magna cupidatat sint consequat sint. Veniam ullamco enim consectetur minim velit nostrud dolor ad. Irure consectetur consectetur nostrud irure ut ullamco veniam deserunt veniam qui minim culpa ex cupidatat.\r\n",
+          "registered": "2021-03-03T05:24:18 -02:00",
+          "latitude": 4.99979,
+          "longitude": 88.387523,
+          "tags": [
+            "Lorem",
+            "tempor",
+            "mollit",
+            "sit",
+            "incididunt",
+            "officia",
+            "pariatur"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Black Jacobs"
+            },
+            {
+              "id": 1,
+              "name": "Barton Daniels"
+            },
+            {
+              "id": 2,
+              "name": "Karyn Shepard"
+            }
+          ],
+          "greeting": "Hello, Patton Holcomb! You have 4 unread messages.",
+          "favoriteFruit": "apple"
+        },
+        {
+          "_id": "66f94e5fc67b47fd43f78a99",
+          "index": 3,
+          "guid": "7ad21fe8-c335-4ab8-b195-f71bd4100f42",
+          "isActive": false,
+          "balance": "$3,489.85",
+          "picture": "http://placehold.it/32x32",
+          "age": 39,
+          "eyeColor": "blue",
+          "name": "Hogan Marshall",
+          "gender": "male",
+          "company": "SCENTRIC",
+          "email": "hoganmarshall@scentric.com",
+          "phone": "+1 (983) 582-3632",
+          "address": "568 Lewis Avenue, Sunbury, Ohio, 7154",
+          "about": "Reprehenderit adipisicing elit exercitation dolore culpa dolore laborum. Anim duis aute culpa occaecat adipisicing velit. Elit sint exercitation consequat nisi pariatur.\r\n",
+          "registered": "2019-05-07T08:10:56 -03:00",
+          "latitude": -27.834359,
+          "longitude": -66.504044,
+          "tags": [
+            "adipisicing",
+            "do",
+            "duis",
+            "laborum",
+            "duis",
+            "cillum",
+            "enim"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Virginia Smith"
+            },
+            {
+              "id": 1,
+              "name": "Alta Houston"
+            },
+            {
+              "id": 2,
+              "name": "Ebony Lane"
+            }
+          ],
+          "greeting": "Hello, Hogan Marshall! You have 3 unread messages.",
+          "favoriteFruit": "strawberry"
+        },
+        {
+          "_id": "66f94e5f719812cd629dfdde",
+          "index": 4,
+          "guid": "b324c3da-0198-452e-83fc-35e2a766c5dc",
+          "isActive": false,
+          "balance": "$3,823.44",
+          "picture": "http://placehold.it/32x32",
+          "age": 36,
+          "eyeColor": "blue",
+          "name": "Clemons Levine",
+          "gender": "male",
+          "company": "OMATOM",
+          "email": "clemonslevine@omatom.com",
+          "phone": "+1 (910) 438-2961",
+          "address": "438 Portland Avenue, Lupton, Pennsylvania, 6007",
+          "about": "Ullamco tempor dolor irure magna adipisicing excepteur ad sint incididunt consectetur deserunt commodo ut. Aliquip et ullamco anim dolor magna aliqua minim dolor velit sunt ut eiusmod. Ex consectetur est ea ut duis labore.\r\n",
+          "registered": "2019-02-20T12:28:44 -02:00",
+          "latitude": 86.466911,
+          "longitude": -115.442377,
+          "tags": [
+            "deserunt",
+            "sunt",
+            "ullamco",
+            "qui",
+            "amet",
+            "mollit",
+            "laborum"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Corine Francis"
+            },
+            {
+              "id": 1,
+              "name": "Stanton Hancock"
+            },
+            {
+              "id": 2,
+              "name": "Janis Bradshaw"
+            }
+          ],
+          "greeting": "Hello, Clemons Levine! You have 6 unread messages.",
+          "favoriteFruit": "banana"
+        }
+      ]
+]
