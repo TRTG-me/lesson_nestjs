14fbcb1a8651a5c3df83e2a8e0ab1a930cfd6760
TORTUGA-me <mcgradysvt@gmail.com>
2024-10-02 11:30:50 +0300

    a

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService)
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('port');
+  app.useGlobalPipes(new ValidationPipe);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Lesson api"')
+    .setDescription("This api for lesson")
+    .setVersion("1.0")
+    .addTag('API')
+    .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(port);
 }
 bootstrap();
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import {users} from "../moks";
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './models/user.model';
+import * as bcrypt from 'bcrypt'
+import { CreateUserDTO } from './dto';
+import { AppError } from 'src/common/errors';
 
 @Injectable()
 export class UserService {
-    getUsers() {
-        return users;
+    constructor(@InjectModel(User) private readonly userRepository: typeof User){}
+
+    async hashPassword (password){
+        return bcrypt.hash(password, 10)
+    }
+    async findUserbyEmail (email: string){
+        return this.userRepository.findOne({where: {email: email}})
+    }
+
+
+
+    async createUser(dto: CreateUserDTO): Promise<CreateUserDTO> {
+        const existUser = await this.findUserbyEmail(dto.email)
+        if (existUser) throw new BadRequestException(AppError.USER_EXIST);
+        dto.password = await this.hashPassword(dto.password)
+        await this.userRepository.create({
+            firstName: dto.firstName,
+            username: dto.username,
+            email: dto.email,
+            password: dto.password
+        })
+        return dto
     }
 }
