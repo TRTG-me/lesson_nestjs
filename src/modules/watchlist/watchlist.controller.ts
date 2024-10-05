96fbf9cd4dd2ae23396cd7b222e632828273352c
TORTUGA-me <mcgradysvt@gmail.com>
2024-10-05 11:07:31 +0300

    last commit

--- src/modules/watchlist/watchlist.controller.ts
+++ src/modules/watchlist/watchlist.controller.ts
@@ -2,30 +2,27 @@ import { Body, Controller, Delete, Get, Patch, Post, Query, Req, UseGuards } fro
 import { WatchlistService } from './watchlist.service';
 import { WatchListDTO } from './dto';
 import { JwtAuthGuard } from 'src/guards/jwt_guard';
+import { CreateAssetResponse } from './response';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('watchlist')
 export class WatchlistController {
     constructor(private readonly watchListService: WatchlistService){}
-
+    @ApiTags('API')
+    @ApiResponse({status: 201, type: CreateAssetResponse})
     @UseGuards(JwtAuthGuard)
     @Post('create')
-    createAsset(@Body() assetDTO:WatchListDTO, @Req() request){
+    createAsset(@Body() assetDTO:WatchListDTO, @Req() request): Promise<CreateAssetResponse>{
         const user = request.user
         return this.watchListService.createAsset(user, assetDTO)
     }
-
-    @Get('get-all')
-    getAllAssets(){
-        return
-    }
-
-    @Patch('update')
-    updateAsset(){
-        return
-    }
+    @ApiTags('API')
+    @ApiResponse({status: 200})
+    @UseGuards(JwtAuthGuard)
     @Delete()
-    deleteAsset(@Query('id') id:string){
-        return
+    deleteAsset(@Query('id') assetId:string, @Req() request): Promise<boolean>{
+        const {id} = request.user
+        return this.watchListService.deleteAsset(id, assetId)
     }
 
 }
--- src/modules/watchlist/watchlist.service.ts
+++ src/modules/watchlist/watchlist.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { WatchList } from './models/watchlist.model';
+import { CreateAssetResponse } from './response';
 
 @Injectable()
 export class WatchlistService {
     constructor(@InjectModel(WatchList) private readonly WatchListRepository: typeof WatchList){}
-    async createAsset(user, dto){
+    async createAsset(user, dto): Promise<CreateAssetResponse>{
         const watchList = {
             user: user.id,
             name: dto.name,
@@ -15,5 +16,10 @@ export class WatchlistService {
         await this.WatchListRepository.create(watchList)
         return watchList
 
+    }
+
+    async deleteAsset(userId: number, assetId: string): Promise<boolean>{
+        await this.WatchListRepository.destroy({where:{id: assetId, user: userId}})
+        return true
     }
 }
